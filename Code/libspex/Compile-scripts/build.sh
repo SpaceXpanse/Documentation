5edd4a8b4f6589433e643533a85f0f8643093c5a
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2023-04-25 13:24:46 +0200

    Add code

---
+++ Code/libspex/Compile-scripts/build.sh
@@ -0,0 +1,44 @@
+pacman -Sy
+pacman -S base-devel git mingw-w64-x86_64-toolchain make mingw-w64-x86_64-cmake autoconf-archive mingw-w64-x86_64-protobuf mingw-w64-x86_64-gtest mingw-w64-x86_64-gflags mingw-w64-x86_64-zeromq mingw-w64-x86_64-openssl mingw-w64-x86_64-glog mingw-w64-x86_64-lmdb mingw-w64-x86_64-lmdbxx mingw-w64-x86_64-sqlite3 mingw-w64-x86_64-libmicrohttpd libtool mingw-w64-x86_64-autotools wget
+git clone https://github.com/RyuMaster/argtable2.git
+cd argtable2
+cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX
+make -j2
+make install
+cp ./src/argtable2.h /home/../mingw64/include/argtable2.h
+cd ..
+git clone https://github.com/RyuMaster/libjson-rpc-cpp.git
+cd libjson-rpc-cpp
+git checkout listen-locally-0.7.0
+mkdir win32-deps
+mkdir win32-deps/include
+cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX -DREDIS_SERVER=NO -DREDIS_CLIENT=NO  -DCOMPILE_STUBGEN=YES -DCOMPILE_EXAMPLES=NO -DCOMPILE_TESTS=NO -DBUILD_STATIC_LIBS=YES -DHUNTER_ENABLED=YES -DCMAKE_CXX_FLAGS="-fpermissive"
+make -j2
+make install
+cd dist
+cp -r ./ /home/../mingw64/
+cd ~/
+curl -o /home/../mingw64/lib/pkgconfig/libglog.pc https://raw.githubusercontent.com/spacexpanse/documentation/main/code/libspex/Compile-scripts/libglog.pc
+curl -o /home/../mingw64/lib/pkgconfig/lmdb.pc https://raw.githubusercontent.com/spacexpanse/documentation/main/code/libspex/Compile-scripts/lmdb.pc
+git clone https://github.com/bitcoin-core/secp256k1.git
+cd secp256k1
+./autogen.sh
+./configure --disable-tests --disable-benchmark --enable-module-recovery
+make
+make install
+cd ..
+git clone https://github.com/xaya/eth-utils.git
+cd eth-utils
+./autogen.sh
+./configure
+make
+make install
+cd ..
+git clone https://github.com/xaya/libxayagame.git
+cd libxayagame
+curl -o configure_patch.diff https://raw.githubusercontent.com/spacexpanse/documentation/main/code/libspex/Compile-scripts/configure_patch.diff
+patch --merge configure.ac configure_patch.diff
+./autogen.sh
+./configure
+make -j2
+make install
